4da72904de2a411032e24bac5213721aacdc2484
eug48 <eug48.github@gmail.com>
2021-11-06 22:45:02 +1100

    support android embedding v2

--- android/src/main/kotlin/com/example/jpegtran_ffi/JpegtranFfiPlugin.kt
+++ android/src/main/kotlin/com/example/jpegtran_ffi/JpegtranFfiPlugin.kt
@@ -1,18 +1,36 @@
 package com.example.jpegtran_ffi
 
+import androidx.annotation.NonNull
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
-class JpegtranFfiPlugin: MethodCallHandler {
+class JpegtranFfiPlugin: FlutterPlugin, MethodCallHandler {
   companion object {
     @JvmStatic
     fun registerWith(registrar: Registrar) {
       val channel = MethodChannel(registrar.messenger(), "jpegtran_ffi")
       channel.setMethodCallHandler(JpegtranFfiPlugin())
     }
+  }
+
+  /// The MethodChannel that will the communication between Flutter and native Android
+  ///
+  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+  /// when the Flutter Engine is detached from the Activity
+  private lateinit var channel : MethodChannel
+
+  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "jpegtran_ffi")
+    channel.setMethodCallHandler(this)
+  }
+
+  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+    channel.setMethodCallHandler(null)
   }
 
   override fun onMethodCall(call: MethodCall, result: Result) {
